1d410be895d7080f9d287015a3a81e5939c4ffca
dimaswift <dimaswift@gmail.com>
2016-09-17 00:23:10 +0300

    updated code generator

--- Editor/DUIPanelEditor.cs
+++ Editor/DUIPanelEditor.cs
@@ -13,7 +13,9 @@ namespace DynamicUI
             base.OnInspectorGUI();
             if(GUILayout.Button("Generate Code"))
             {
-                GenerateCode();
+
+                var parser = new ClassParser();
+                parser.Test();
             }
             if (GUILayout.Button("Bind Elements"))
             {
@@ -26,17 +28,19 @@ namespace DynamicUI
             string[] directives = new string[] { "UnityEngine", "DynamicUI", "UnityEngine.UI" };
             string[] regions = new string[] { "Elements" };
             string[] inherited = new string[] { "DUIPanel"};
-           
-            var script = System.IO.File.CreateText(Application.dataPath + "/Scripts/" + panel.name + ".cs");
-            var cls = new Class(Member.ProtectionLevel.Public, Class.ClassType.Sealed, "Bae", inherited, directives, regions);
-            cls.AddMember(new Class("Class1"));
-            cls.AddMember(new Class(Member.ProtectionLevel.Public, "Class2").AddRegion("Ass"));
-            cls.AddMember(new Class(Member.ProtectionLevel.Public, Class.ClassType.Partial, "Class3").AddInherited("Class1"));
-            cls.AddMember(new Class(Member.ProtectionLevel.Public, Class.ClassType.Partial, "Class4", inherited, null, null, "Cool").AddRegion("Cool").AddMember(new Field(Member.ProtectionLevel.None, "string", "assh", null, "Cool")));
-            script.Write(cls.ToString(0));
+            var file = Application.dataPath + "/Scripts/" + panel.name + ".cs";
+
+            var script = System.IO.File.OpenText(file);
+            
+            var parser = new ClassParser();
+            parser.Test();
+            var cls = parser.Parse(script.ReadToEnd());
             script.Close();
+            //   var cls = new Class(Member.ProtectionLevel.Public, Class.ClassType.Sealed, "Bae", inherited, directives, regions);
+            var script2 = System.IO.File.CreateText(Application.dataPath + "/Scripts/" + panel.name + ".cs");
+              script2.Write(cls.ToString(0));
+            script2.Close();
+         
         }
     }
-
-
 }
